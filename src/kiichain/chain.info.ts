48733f0cefe95877dbf2416978e2ae9f04504302
Jhelison Uchoa <jhelisong@gmail.com>
2025-01-17 14:07:48 -0300

    feat: Update the network to testnet ORO

--- src/kiichain/chain.info.ts
+++ src/kiichain/chain.info.ts
@@ -1,11 +1,11 @@
 import { defineChain } from "viem"
 
 const kiichain = defineChain({
-  id: 123454321,
-  name: "Kiichain Testnet",
+  id: 0x538,
+  name: "Kiichain Oro",
   nativeCurrency: { name: "Ether", symbol: "kii", decimals: 18 },
   rpcUrls: {
-    default: { http: ["https://a.sentry.testnet.kiivalidator.com:8645"] },
+    default: { http: ["https://json-rpc.uno.sentry.testnet.v3.kiivalidator.com"] },
   },
   blockExplorers: {
     default: { name: "Kii Explorer", url: "https://app.kiichain.io/kiichain" },
